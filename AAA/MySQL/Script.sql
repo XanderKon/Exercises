b02ea06ebf31eb825eba86aed587db36136864e0
Xander <aleks@costlin.com>
2016-02-04 15:24:09 +0300

    minor fixes

--- AAA/MySQL/Script.sql
+++ AAA/MySQL/Script.sql
@@ -1,3 +1,4 @@
+-- Create user table
 CREATE TABLE `users` (
   `id` int AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(255)
@@ -6,6 +7,7 @@ CREATE TABLE `users` (
 -- Dump data
 INSERT INTO `users` (name) VALUES ("Sasha"), ("Andrey"), ("Sergey"), ("Oleg"), ("Anton");
 
+-- Create relationship
 CREATE TABLE `relationship` (
   `id` int AUTO_INCREMENT PRIMARY KEY,
   `id_user` int,
@@ -17,25 +19,38 @@ CREATE TABLE `relationship` (
 -- Dump data
 INSERT INTO `relationship` (`id_user`, `id_friend`) VALUES (1, 2), (2, 1), (1, 5), (2, 4), (4, 2), (4, 5);
 
+-- Users who have more than 5 friends
+SELECT u.name
+FROM users u
+INNER JOIN `relationship` r ON r.id_user = u.id
+GROUP BY u.id
+HAVING count(u.id) > 5;
+
+-- Best friends :)
+
 -- First
-SELECT DISTINCT u.`NAME`, u1.`NAME` as 'Friend' FROM users u, users u1, (
-SELECT r.id_user, r1.id_friend FROM relationship r
-INNER JOIN relationship r1 ON r.id_user > r1.id_friend AND r1.id_user < r.id_friend
-GROUP BY r.id_user
-) rel WHERE u.id = rel.id_user AND u1.id = rel.id_friend
+SELECT DISTINCT u.`NAME`, u1.`NAME` as 'Friend'
+FROM users u, users u1, (
+  SELECT r.id_user, r1.id_friend FROM relationship r
+  INNER JOIN relationship r1 ON r.id_user > r1.id_friend AND r1.id_user < r.id_friend
+  GROUP BY r.id_user
+) rel WHERE u.id = rel.id_user AND u1.id = rel.id_friend;
 
 -- Second (without JOINs)
 SELECT u.`NAME`, u1.`NAME` as 'Friend'
 FROM
 users u, users u1, (
-	SELECT DISTINCT r.id_user, r.id_friend
-	FROM relationship r
-	WHERE NOT EXISTS (SELECT * FROM relationship r1 WHERE r.id_user = r1.id_friend AND r1.id_user <> r.id_friend)
-) rel WHERE u.id = rel.id_user AND u1.id = rel.id_friend
+  SELECT DISTINCT r.id_user, r.id_friend
+  FROM relationship r
+  WHERE NOT EXISTS (
+      SELECT * FROM relationship r1 WHERE r.id_user = r1.id_friend AND r1.id_user <> r.id_friend
+  )
+) rel WHERE u.id = rel.id_user AND u1.id = rel.id_friend;
 
 -- Third
 SELECT DISTINCT u.`NAME`, u1.`NAME` as 'Friend' FROM users u, users u1, (
-  SELECT r.id_user, r1.id_friend FROM relationship r
-    INNER JOIN relationship r1 ON r.id_user > r1.id_friend AND r1.id_user < r.id_friend
+  SELECT r.id_user, r1.id_friend
+  FROM relationship r
+  INNER JOIN relationship r1 ON r.id_user > r1.id_friend AND r1.id_user < r.id_friend
   GROUP BY r.id_user
-) rel WHERE u.id = least(rel.id_user, rel.id_friend) AND u1.id = greatest(rel.id_user, rel.id_friend)
+) rel WHERE u.id = least(rel.id_user, rel.id_friend) AND u1.id = greatest(rel.id_user, rel.id_friend);
